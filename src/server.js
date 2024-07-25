fc69eb377fbfd6d5ba19aae9c085ed7a44ae5ea2
TrinhNamTrung <trungremtry@gmail.com>
2024-07-25 19:54:47 +0700

    Nodejs-#21

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
 
 const express = require('express');
 const path = require("path");
+require('dotenv').config()
+
+// --------------------------------------------------------------
+
 const app = express()
-const port = 8085
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 // config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// config static file
+app.use(express.static(path.join(__dirname, "public")));
+
+// config routes
 app.get('/', (req, res) => {
     res.render("sample.ejs");
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
