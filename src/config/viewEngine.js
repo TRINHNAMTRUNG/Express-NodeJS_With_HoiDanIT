56e72a8863270e10481fec2a3cf5c44cbc6716d8
TrinhNamTrung <trungremtry@gmail.com>
2024-07-26 17:40:07 +0700

    Nodejs-#24

---
+++ src/config/viewEngine.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express")
+const configViewEngine = (app) => {
+    // config template engine
+    app.set('views', path.join("./src", "views"));
+    app.set('view engine', 'ejs');
+    // config static file
+    /*chỉ định một thư mục cụ thể mà từ đó các tệp tĩnh sẽ được phục vụ. 
+Người dùng có thể truy cập vào các tệp trong thư mục này thông qua trình duyệt của họ. 
+Điều này có nghĩa là bạn đang xác định rõ ràng thư mục nào sẽ được cấp phép để cung cấp các tệp tĩnh cho người dùng. */
+    app.use(express.static(path.join("./src", "public")));
+}
+
+module.exports = configViewEngine;
--- src/server.js
+++ src/server.js
@@ -1,26 +1,23 @@
 
 const express = require('express');
-const path = require("path");
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 require('dotenv').config()
 
 // --------------------------------------------------------------
+const app = express();
 
-const app = express()
+// Thiết lập các biến môi trường trong env, giúp tái sử dụng biến và tiện lợi cho thay đổi giá trị các biến môi trường tại một nơi
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
 // config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// config static file
-app.use(express.static(path.join(__dirname, "public")));
+configViewEngine(app);
 
 // config routes
-app.get('/', (req, res) => {
-    res.render("sample.ejs");
-})
+app.use("/", webRoutes);
 
+/*Khởi động UNIX Socket và lắng nghe các kết nối trên các path*/
 app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
