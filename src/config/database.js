3cc6f5e64aa09fb84243ac637134a5d6e0f7a6e4
TrinhNamTrung <trungremtry@gmail.com>
2024-07-28 14:10:17 +0700

    Nodejs-#39

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // get the client
 const connection = mysql.createPool({
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,11 @@
 const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
     return res.render("home.ejs");
+}
+
+const getCreatePage = (req, res)=> {
+    res.render('create.ejs');
 }
 
 const getAbc = (req, res) => {
@@ -13,25 +18,32 @@ const getAbc = (req, res) => {
             res.send(JSON.stringify(results));
         }
     );
+    
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
     const data = req.body;
     let name = data.name;
     let email = data.email;
     let city = data.city;
-    connection.query(
-        `INSERT INTO Users (email, name, city)
-        VALUES (?, ?, ?)`,
-        [email, name, city],
-        function (err, results, fields) {
-            console.log(">>>> res ", results); // results contains rows returned by server
-            console.log("create user is success");
-        }
+    // connection.query(
+    //     `INSERT INTO Users (email, name, city)
+    //     VALUES (?, ?, ?)`,
+    //     [email, name, city],
+    //     function (err, results, fields) {
+    //         console.log(">>>> res ", results); // results contains rows returned by server
+    //         console.log("create user is success");
+    //     }
+    // );
+    const [results, fields] = await connection.query(
+        `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+        [email, name, city]
     );
     res.send("create a new user");
 }
 
+
 module.exports = {
-    getHomePage, getAbc, postCreateUser
+    getHomePage, getAbc, postCreateUser,
+    getCreatePage
 }
